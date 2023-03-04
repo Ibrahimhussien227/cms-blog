6ca525a31515fe6b21d99515271b88fe52ab77ef
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2023-03-04 23:18:07 +0300

    refactor

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,4 +1,6 @@
-<?php if (isset($_POST["checkBoxArray"])) {
+<?php
+
+if (isset($_POST["checkBoxArray"])) {
   foreach ($_POST["checkBoxArray"] as $postValueId) {
     $bulk_options = $_POST["bulk_options"];
 
@@ -149,7 +151,19 @@
         echo "<td>{$post_date}</td>";
         echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
         echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-        echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
+        ?>
+
+        <form method="post">
+
+        <input type="hidden" name="post_id" value="<?php echo $post_id; ?>">
+
+        <?php echo '<td><input class="btn btn-danger" type="submit" name="delete" value="Delete"></td>'; ?>
+
+
+        </form>
+
+        <?php
+        // echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
         echo "<td><a href='posts.php?reset={$post_id}'>{$post_view_count}</a></td>";
         echo "</tr>";
 
@@ -161,13 +175,13 @@
 </form>
 
 <?php
-if (isset($_GET["delete"])) {
-  $post_id = escape($_GET["post_id"]);
+if (isset($_POST["delete"])) {
+  $post_id = escape($_POST["post_id"]);
 
   $query = "DELETE FROM posts WHERE post_id = {$post_id} ";
   $delete_query = mysqli_query($connection, $query);
 
-  header("Location: posts.php");
+  header("Location: /cms/admin/posts.php");
 
   confirmQuery($delete_query);
 }
